cc1c04ddd09722d1b3a4f0d65e30707dc72b2257
Sutantu Balamurugan <sutan04@gmail.com>
2025-06-11 19:33:11 -0700

    Landing/Login page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      // Add other folders where you use Tailwind classes
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
