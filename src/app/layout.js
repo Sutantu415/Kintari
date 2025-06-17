0001e70652ec4cce83ada59fd4906ca086940224
Sutantu Balamurugan <sutan04@gmail.com>
2025-06-16 19:11:33 -0700

    Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "context/AuthContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +28,8 @@ export default function RootLayout({ children }) {
       >
         <AuthContextProvider>
           {children}
+          <SpeedInsights />
+          <Analytics />
         </AuthContextProvider>
       </body>
     </html>
